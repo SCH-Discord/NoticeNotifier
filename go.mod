8aa751206f84cbca6016a4dd249fdb371dbb51da
found-cake <skymin468@gmail.com>
2024-02-14 23:52:31 +0900

    scheduling

--- go.mod
+++ go.mod
@@ -1,3 +1,20 @@
 module github.com/SCH-Discord/NoticeNotifier
 
 go 1.21
+
+require (
+	github.com/chromedp/cdproto v0.0.0-20240202021202-6d0b6a386732 // indirect
+	github.com/chromedp/chromedp v0.9.5 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.3.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	gorm.io/driver/mysql v1.5.4 // indirect
+	gorm.io/gorm v1.25.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,58 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
+func main() {
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	fmt.Println("Ctrl + C를 통해 종료할 수 있습니다.")
+
+	for {
+		select {
+		case <-done:
+			fmt.Println("프로그램을 종료합니다.")
+			os.Exit(0)
+		case <-time.After(timeUntilNextRun()):
+			doTask()
+		}
+	}
+}
+
+func doTask() {
+	//TODO
+	fmt.Println("test")
+}
+
+// 다음 실행 시간 구하기
+var targetHours = []int{10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+
+func nextRunHour(currentHour int) int {
+	for _, h := range targetHours {
+		if h > currentHour {
+			return h
+		}
+	}
+	return targetHours[0]
+}
+
+// 다음 실행 시간 까지 남은 시간리턴
+func timeUntilNextRun() time.Duration {
+	now := time.Now()
+	targetTime := time.Date(now.Year(), now.Month(), now.Day(), nextRunHour(now.Hour()), 0, 0, 0, now.Location())
+
+	if now.After(targetTime) {
+		targetTime = targetTime.Add(24 * time.Hour)
+	}
+
+	log.Println("다음실행 시간:", targetTime)
+
+	return targetTime.Sub(now)
 }
