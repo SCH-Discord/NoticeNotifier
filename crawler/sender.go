7e1d41255cb25c4624cba022405a4c597d9fe76f
found-cake <skymin468@gmail.com>
2024-02-16 22:32:53 +0900

    url change

--- crawler/sender.go
+++ crawler/sender.go
@@ -11,7 +11,7 @@ import (
 func Send(name string, subscriber *model.Subscriber, embeds *[]webhook.Embed) {
 	err := webhook.SendMessage(subscriber.URL, &webhook.Message{
 		Username:  name,
-		AvatarUrl: "https://raw.githubusercontent.com/SCH-Discord/.github/main/profile/logo.png",
+		AvatarUrl: "https://raw.githubusercontent.com/SCH-Discord/image/main/profile.jpg",
 		Embeds:    embeds,
 	})
 	if err == nil {
