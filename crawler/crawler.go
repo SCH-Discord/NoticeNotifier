1890c1148b7ec74fb9d39b7fe8b1f96b707a1563
found-cake <skymin468@gmail.com>
2024-02-17 18:37:06 +0900

    refactor

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -1,12 +1,23 @@
 package crawler
 
 import (
+	"context"
 	"errors"
+	"fmt"
 	"github.com/SCH-Discord/NoticeNotifier/database"
 	"github.com/SCH-Discord/NoticeNotifier/database/model"
 	"github.com/SCH-Discord/NoticeNotifier/webhook"
+	"github.com/chromedp/chromedp"
 	"log"
+	"time"
 )
+
+func CreateCrawler() (context.Context, context.CancelFunc, context.CancelFunc) {
+	allocatorContext, allocatorCancel := chromedp.NewRemoteAllocator(context.Background(), "wss://chrome:9222")
+	ctx, ctxCancel := chromedp.NewContext(allocatorContext)
+
+	return ctx, allocatorCancel, ctxCancel
+}
 
 func Send(name string, subscriber *model.Subscriber, embeds *[]webhook.Embed) {
 	err := webhook.SendMessage(subscriber.URL, &webhook.Message{
@@ -24,4 +35,19 @@ func Send(name string, subscriber *model.Subscriber, embeds *[]webhook.Embed) {
 	} else {
 		log.Println(err)
 	}
+}
+
+var day int
+var date string
+
+func NowDate() string {
+	now := time.Now()
+	if date == "" {
+		day = now.Day()
+		date = fmt.Sprintf("%d-%02d-%02d", now.Year(), now.Month(), now.Day())
+	} else if day != now.Day() {
+		day = now.Day()
+		date = fmt.Sprintf("%d-%02d-%02d", now.Year(), now.Month(), now.Day())
+	}
+	return date
 }
