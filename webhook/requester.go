453e1a5fb92769e6f2ac40cabb573ee66b66869f
found-cake <skymin468@gmail.com>
2024-02-18 18:35:07 +0900

    fix 최대 임베드 수가 10개로 되어 있기 때문에 안정성을 위한 수정

--- webhook/requester.go
+++ webhook/requester.go
@@ -10,6 +10,8 @@ const webhookSuccess = 204
 
 // 디스코드 웹후크를 전송
 func SendMessage(url string, message *Message) error {
+	newM := checkMessage(message)
+
 	payload := new(bytes.Buffer)
 
 	if err := json.NewEncoder(payload).Encode(message); err != nil {
@@ -32,8 +34,28 @@ func SendMessage(url string, message *Message) error {
 		return &NotOk{resp.StatusCode()}
 	}
 
+	if newM != nil {
+		go SendMessage(url, newM)
+	}
+
 	fasthttp.ReleaseRequest(req)
 	fasthttp.ReleaseResponse(resp)
 
 	return nil
+}
+
+func checkMessage(message *Message) *Message {
+	if message.Embeds == nil || len(*message.Embeds) <= 10 {
+		return nil
+	}
+
+	a := (*message.Embeds)[10:]
+	b := (*message.Embeds)[:10]
+
+	newMessage := *message
+	newMessage.Embeds = &a
+
+	message.Embeds = &b
+
+	return &newMessage
 }
