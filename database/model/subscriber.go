a2b71a7767bafdde5c49318a8cc888940b77a1e4
found-cake <skymin468@gmail.com>
2024-02-28 13:43:15 +0900

    remove instagram

--- database/model/subscriber.go
+++ database/model/subscriber.go
@@ -1,13 +1,12 @@
 package model
 
 type Subscriber struct {
-	ID        uint64 `gorm:"primaryKey"`
-	URL       string `gorm:"unique;not null"`
-	Main      bool   `gorm:"not null"`
-	Library   bool   `gorm:"not null"`
-	Instagram bool   `gorm:"not null"`
-	Student   bool   `gorm:"not null"`
-	SW        bool   `gorm:"not null"`
+	ID      uint64 `gorm:"primaryKey"`
+	URL     string `gorm:"unique;not null"`
+	Main    bool   `gorm:"not null"`
+	Library bool   `gorm:"not null"`
+	Student bool   `gorm:"not null"`
+	SW      bool   `gorm:"not null"`
 }
 
 func (s *Subscriber) TableName() string {
